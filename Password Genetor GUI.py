45d393ee61042b401a5909323b9660b43d973abd
Wwahib <90974026+wwahib@users.noreply.github.com>
2021-11-03 16:07:50 +0530

    Update Password Genetor GUI.py

--- Password Genetor GUI.py
+++ Password Genetor GUI.py
@@ -18,31 +18,31 @@ root=Tk()
 root.title('#WELCOME TO THE PASSWOARD GENERATOR#')
 root.geometry("500x300")
 
-#Creating the label frame
+										#Creating the label frame
 lf=LabelFrame(root,text='How many Characters:')
 lf.pack(pady=20)
 
-#creating the Entry box 
+										#Creating the Entry box 
 char_input=Entry(lf,text='HELLO',bg="#fb743e",font=("helvetica",20,))
 char_input.pack(pady=20,padx=20)
 char_input.insert(0, "12")
 char_input.focus()
 
-#creating the entery box for retuend password
+								#Creating the entery box for retuend password
 password_field=Entry(root,text='',font=("helvetica",20))
 password_field.pack(pady=20)
 
-#creating the frame of a button AND CLIP BOARD 
+									#Creating the frame of a button AND CLIP BOARD 
 my_frame_button_clip=Frame(root)
 my_frame_button_clip.pack(pady=20)
 
-#										Creating the button 
+										#Creating the button 
 my_button=Button(my_frame_button_clip,text='GENERATOR STRONG PASSWOARD',command=password_genetor)
 my_button.grid(row=0,column=0,padx=10)
 
-# #creating the clipboard button 
-# my_clip=Button(my_frame_button_clip,text='Copy to the clip Board')
-# my_clip.grid(row=0,column=1,padx=10)
+									#Creating the clipboard button 
+my_clip=Button(my_frame_button_clip,text='Copy to the clip Board')
+my_clip.grid(row=0,column=1,padx=10)
 
 
 root.mainloop()
